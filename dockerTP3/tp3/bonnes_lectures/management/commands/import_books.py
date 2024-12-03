2a9814b8c681839663f3ebb674de16feec30bc5a
oz <zieabdoulaye11@gmail.com>
2024-12-03 15:53:00 +0100

    Ajout de Bootstrap sur all_books

--- dockerTP3/tp3/bonnes_lectures/management/commands/import_books.py
+++ dockerTP3/tp3/bonnes_lectures/management/commands/import_books.py
@@ -3,25 +3,17 @@ from django.core.management.base import BaseCommand
 from bonnes_lectures.models import Book
 
 class Command(BaseCommand):
-    help = 'Import books from a custom CSV file'
+    help = "Import books from a CSV file"
 
-    def add_arguments(self, parser):
-        parser.add_argument('csv_file', type=str, help='Path to the CSV file')
-
-    def handle(self, *args, **kwargs):
-        csv_file = kwargs['csv_file']
-        with open(csv_file, newline='', encoding='utf-8') as file:
-            reader = csv.DictReader(file, delimiter='|')
+    def handle(self, *args, **options):
+        with open('bonnes_lectures/data/manga.csv', newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
             for row in reader:
-                book, created = Book.objects.get_or_create(
-                    title=row['Titre'],
-                    publisher=row['Maison d\'édition'],
-                    year=int(row['Publication']),
-                    isbn=row['ISBN'],
-                    backCover=row['Résumé'],
-                    cover=True  # Si vous voulez gérer cette colonne, ajoutez-la dans le CSV
+                Book.objects.create(
+                    title=row['title'],
+                    publisher=row['publisher'],
+                    year=row['year'],
+                    isbn=row['isbn'],
+                    backCover=row['back_cover']
                 )
-                if created:
-                    self.stdout.write(self.style.SUCCESS(f"Book '{book.title}' added."))
-                else:
-                    self.stdout.write(f"Book '{book.title}' already exists.")
+        self.stdout.write(self.style.SUCCESS('Données importées avec succès !'))
